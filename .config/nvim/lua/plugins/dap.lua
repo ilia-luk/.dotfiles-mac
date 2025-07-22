c1e0ad15439940369c1f9a9fda73847d1b9d8012
ilia-luk <ilia@domusnetwork.io>
2025-07-22 14:00:21 +0700

    testing dap for rust

--- .config/nvim/lua/plugins/dap.lua
+++ .config/nvim/lua/plugins/dap.lua
@@ -1,8 +1,35 @@
--- return {
---   "mfussenegger/nvim-dap",
---   opts = function()
---     local dap = require("dap")
---     dap.adapters["pwa-node"].host = "127.0.0.1"
---     dap.adapters["node"].host = "127.0.0.1"
---   end,
--- }
+return {
+  "mfussenegger/nvim-dap",
+  opts = function()
+    local dap = require("dap")
+    dap.adapters["pwa-node"].host = "127.0.0.1"
+    dap.adapters["node"].host = "127.0.0.1"
+    dap.adapters.lldb = {
+      type = "executable",
+      command = "/opt/homebrew/bin/lldb-vscode", -- adjust as needed
+      name = "lldb",
+    }
+    dap.configurations.rust = {
+      {
+        name = "hello-world",
+        type = "lldb",
+        request = "launch",
+        program = function()
+          return vim.fn.getcwd() .. "/target/debug/hello-world"
+        end,
+        cwd = "${workspaceFolder}",
+        stopOnEntry = false,
+      },
+      {
+        name = "hello-dap",
+        type = "lldb",
+        request = "launch",
+        program = function()
+          return vim.fn.getcwd() .. "/target/debug/hello-dap"
+        end,
+        cwd = "${workspaceFolder}",
+        stopOnEntry = false,
+      },
+    }
+  end,
+}
